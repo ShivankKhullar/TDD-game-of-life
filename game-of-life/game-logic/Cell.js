e7cae2f0c7ce88cb52f2612d5596d6e32d613315
ShivankKhullar <2224236@brunel.ac.uk>
2023-05-19 11:34:41 +0100

    Cell-Test-Working

---
+++ game-of-life/game-logic/Cell.js
@@ -0,0 +1,24 @@
+import CellState from "./../game-logic/CellState.js";
+
+export default class Cell{
+    constructor (state) {
+        if (Object.values(CellState).indexOf(state) === -1) {
+            throw new Error('Invalid State');
+        }
+        this.state = state;
+    }
+
+    getNextState(numNeighbors){
+        if (this.state === CellState.ALIVE){
+            if (numNeighbors === 2 || numNeighbors === 3){
+                return this.state; // Which will be alive
+            }
+        } 
+        else {
+            if (numNeighbors === 3){
+                return CellState.ALIVE;
+            }
+        }        
+        return CellState.DEAD; 
+    }
+}
